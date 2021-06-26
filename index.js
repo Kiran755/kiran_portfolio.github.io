3f22f66bb6c22b85c6064855c82e3c6e5967fa52
Kiran755 <kirandevadiga441@gmail.com>
2021-06-27 01:49:29 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,7 @@
+var a = document.getElementById("menu");
+// a.addEventListener("onclick",function(){
+//     a.style.backgroundColor ="yellow";
+// });
+function Show(){
+    a.classList.toggle("menu");
+}
